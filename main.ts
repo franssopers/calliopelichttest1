050fdadd49326292983b8fefe9260085ae6e5dfd
franssopers <50528638+franssopers@users.noreply.github.com>
2024-04-27 09:46:36 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    l = input.lightLevel()
+    basic.showNumber(l)
+    basic.pause(2000)
+})
+let l = 0
+l = 0
+led.setBrightness(13)
+basic.showString("houdoe")
